fbf1ee798ac8ec75a7584c9d9c39469ad101381d
Anderson Viana <andviana@hotmail.com>
2022-02-19 19:58:09 -0300

    implementaçao layout dashboard

---
+++ pages/dashboard.tsx
@@ -0,0 +1,48 @@
+import Grid from '@mui/material/Grid'
+import Paper from '@mui/material/Paper'
+import React from 'react'
+import Chart from '../components/Chart'
+import Deposits from '../components/Deposits'
+import Layout from '../components/Layout'
+import Orders from '../components/Orders'
+
+const dashboard = () => {
+    return (
+        <Layout>
+            {/* Chart */}
+            <Grid item xs={12} md={8} lg={9}>
+                <Paper
+                    sx={{
+                        p: 2,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        height: 240,
+                    }}
+                >
+                    <Chart />
+                </Paper>
+            </Grid>
+            {/* Recent Deposits */}
+            <Grid item xs={12} md={4} lg={3}>
+                <Paper
+                    sx={{
+                        p: 2,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        height: 240,
+                    }}
+                >
+                    <Deposits />
+                </Paper>
+            </Grid>
+            {/* Recent Orders */}
+            <Grid item xs={12}>
+                <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+                    <Orders />
+                </Paper>
+            </Grid>
+        </Layout>
+    )
+}
+
+export default dashboard
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Layout from '../components/Layout'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
+    <Layout>
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
@@ -66,6 +68,7 @@ const Home: NextPage = () => {
         </a>
       </footer>
     </div>
+    </Layout>
   )
 }
 
